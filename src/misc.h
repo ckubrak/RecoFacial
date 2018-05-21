d5f4f335094e6f706d095b1654270bd2ecc2fa00
Cristian Kubrak <kubrakcristian@gmail.com>
2018-05-21 16:17:17 -0300

    Sorry, colgue en subir el misc.h

---
+++ src/misc.h
@@ -0,0 +1,28 @@
+#ifndef _MISC_H_
+#define _MISC_H_
+
+#include "imagen.h"
+#include "pca.h"
+#include "knn.h"
+#include "kfold.h"
+
+
+double presicion(int truePos, int falsePos)
+{
+    return double(truePos/(truePos + falsePos));
+}
+double recall(int truePos, int falseNeg)
+{
+    return double(truePos/(truePos + falseNeg));
+}
+
+double accuracy(int truePos, int trueNeg, int falsePos, int falseNeg)
+{
+    return double(truePos + trueNeg /(truePos + trueNeg + falsePos + falseNeg));
+}
+double mediaArmoncia(double presicion, double recall)
+{
+    return 2 * (presicion*recall/presicion + recall);
+}
+
+#endif
